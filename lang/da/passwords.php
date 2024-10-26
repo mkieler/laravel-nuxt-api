77fed467d6b0cba7f98293168cea815abeed3bac
MKieler <mattias@mkieler.com>
2024-10-26 19:49:55 +0200

    Translations

---
+++ lang/da/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Din adgangskode er blevet nulstillet',
+    'sent' => 'Vi har sendt en e-mail med et link til nulstilling af din adgangskode',
+    'throttled' => 'Vent venligst før du prøver igen.',
+    'token' => 'Denne nulstillingskode til adgangskode er ugyldig.',
+    'user' => "Vi kan ikke finde en bruger med den e-mail-adresse.",
+
+];
