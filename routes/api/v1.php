f76a70ab1721ac8b33a21232dcdfabd76e42ce37
MKieler <mattias@mkieler.com>
2024-10-28 11:05:39 +0100

    Test data for data table

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -6,3 +6,22 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('user', [UserController::class, 'getAuthenticatedUser']);
+
+Route::get('test-data', function () {
+    sleep(10);
+    return response()->json([
+        [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'age' => 30,
+        ],[
+            'name' => 'Jane Doe',
+            'email' => 'janedoe@example.com',
+            'age' => 25,
+        ],[
+            'name' => 'Harry Potter',
+            'email' => 'hp@example.com',
+            'age' => 15,
+        ]
+    ]);
+});
